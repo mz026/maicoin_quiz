82b6223fadf5d66c77d82ea8a41d7986b5033636
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2014-09-21 10:53:30 +0800

    add LinkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,17 @@
 class LinkedList
+  def initialize 
+    @index = 0
+    @nodes = []
+  end
 
+  def next
+    current_node = @nodes[@index]
+    @index += 1 if @nodes[@index]  
+
+    current_node
+  end
+
+  def add node
+    @nodes << node
+  end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,5 +1,36 @@
 require 'lib/linked_list'
 
 describe LinkedList do
-  
+  let(:list) { LinkedList.new }
+
+  describe '::new()' do
+    it "takes no argument to init" do
+      list = LinkedList.new 
+    end
+  end
+
+  describe '#next' do
+    it "returns nil when init" do
+      expect(list.next).to be_nil
+    end
+  end
+
+  describe '#add(node)' do
+    let(:node1) { double(:node1) }
+    let(:node2) { double(:node2) }
+
+    it 'adds the node into list to the tail of list' do
+      expect(list.next).to be_nil
+
+      list.add(node1)
+      expect(list.next).to eq(node1)
+
+      list.add(node2)
+      expect(list.next).to eq(node2)
+
+      expect(list.next).to be_nil
+    end
+  end
+
+
 end
